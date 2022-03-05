5ed76e2ef63bdbf00fbbfb3d829eb7f9ff5c1fa2
Changwan Jun <wan2land@gmail.com>
2022-03-05 22:00:41 +0900

    fix: eslint@8 support

--- tests/rules.test.js
+++ tests/rules.test.js
@@ -1,32 +1,33 @@
 const path = require('path')
 
-const { CLIEngine } = require('eslint')
+const { ESLint } = require('eslint')
 
 
 describe('eslint javascript rules', () => {
-  it('test sample', () => {
-    const cli = new CLIEngine({
+  it('test sample', async () => {
+    const cli = new ESLint({
       useEslintrc: false,
-      configFile: path.resolve(__dirname, 'javascript/.eslintrc.js'),
+      overrideConfigFile: path.resolve(__dirname, 'javascript/.eslintrc.js'),
     })
 
-    const result = cli.executeOnFiles(path.resolve(__dirname, 'javascript/sample.js'))
-    const messages = [].concat(...result.results.map((result) => result.messages))
+    const results = await cli.lintFiles(path.resolve(__dirname, 'javascript/sample.js'))
+
+    const messages = [].concat(...results.map((result) => result.messages))
 
     expect(messages).toEqual([])
   })
 })
 
 describe('eslint typescript rules', () => {
-  const cli = new CLIEngine({
+  const cli = new ESLint({
     useEslintrc: false,
-    configFile: path.resolve(__dirname, 'typescript/.eslintrc.js'),
+    overrideConfigFile: path.resolve(__dirname, 'typescript/.eslintrc.js'),
   })
 
   for (const file of ['sample1.ts', 'sample2.ts']) {
-    it(`test ${file}`, () => {
-      const result = cli.executeOnFiles(path.resolve(__dirname, 'typescript', file))
-      const messages = [].concat(...result.results.map((result) => result.messages))
+    it(`test ${file}`, async () => {
+      const results = await cli.lintFiles(path.resolve(__dirname, 'typescript', file))
+      const messages = [].concat(...results.map((result) => result.messages))
   
       expect(messages).toEqual([])
     })  
